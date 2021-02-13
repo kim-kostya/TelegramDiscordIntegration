28ace05704228339468e3f53d5fd8aef4b73ba56
kim-kostya <konakim51@gmail.com>
2021-02-13 22:59:10 +0500

    Fixed install script

--- install.sh
+++ install.sh
@@ -4,10 +4,10 @@ if not [ -f "/lib/tdi" ]; then
     mkdir "/lib/tdi"
 fi
 
-python ./install.py
+python3 ./install.py
 
 cp ./src/* /lib/tdi
-cp ./tdi.service ./lib/systemd/system/tdi.service
+cp ./tdi.service /lib/systemd/system/tdi.service
 
 chmod 644 /lib/systemd/system/tdi.service
 systemctl enable tdi.service
